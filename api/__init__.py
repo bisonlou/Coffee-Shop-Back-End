b08142d9c7f88f56e84d900004118738b589fec9
innocent lou <bisonlou@gmail.com>
2019-12-03 10:32:14 +0300

    modified cors policy

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,17 @@ from api.database import setup_db
 
 app = Flask(__name__)
 setup_db(app)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}},
+     supports_credentials=True)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+    return response
 
 import api.controllers.drinks
 import api.controllers.errors
